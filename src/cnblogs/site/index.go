095dd98f5482eb1d2620c29dd1853dedc9f0abaa
grj1046 <guorenjun@outlook.com>
2017-06-19 16:53:03 +0800

    add static html site file

--- src/cnblogs/site/index.go
+++ src/cnblogs/site/index.go
@@ -15,14 +15,26 @@ func Main(conf conf.Conf) {
 	if HTTPPort <= 0 {
 		HTTPPort = 8080
 	}
+	fmt.Println("site started on port: " + strconv.Itoa(HTTPPort))
+	//Static Html
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www"))))
+	//API
 	http.HandleFunc("/manage", manageHandler)
+
 	err := http.ListenAndServe(":"+strconv.Itoa(HTTPPort), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
-	fmt.Println("site started on port: " + strconv.Itoa(HTTPPort))
 }
 
 func manageHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello World")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/ing", http.StatusFound)
+}
+
+func ingHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello this is ing page")
 }
